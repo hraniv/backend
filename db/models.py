43dde72dd39aeb002319d878bfab9ed39ed4bbd7
Mark Mishyn <mark.mishyn@gmail.com>
2017-03-12 10:52:49 +0300

    Added AuthToken model

--- db/models.py
+++ db/models.py
@@ -1,4 +1,8 @@
+import binascii
+import os
+
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from model_utils.models import TimeStampedModel
 
 
@@ -12,6 +16,20 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class AuthToken(models.Model):
+    key = models.CharField(max_length=40, primary_key=True)
+    user = models.OneToOneField(User, related_name='auth_token', on_delete=models.CASCADE)
+    created = models.DateTimeField(_("Created"), auto_now_add=True)
+
+    def __str__(self):
+        return self.key
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = binascii.hexlify(os.urandom(20)).decode()
+        return super().save(*args, **kwargs)
 
 
 class Category(TimeStampedModel):
